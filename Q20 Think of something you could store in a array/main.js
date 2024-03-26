2ea90fd898e1ae335ce8e3b5f9161f13fecba123
Nadeem khan <160847092+Nadeemkhan878@users.noreply.github.com>
2024-03-26 18:16:08 +0500

    Add files via upload

---
+++ Q20 Think of something you could store in a array/main.js
@@ -0,0 +1,11 @@
+// 20:Think of something you could store in a array. 
+// For example, you could make a list of mountains, rivers, 
+// countries, cities, languages, or anything else you’d like. 
+// Write a program that creates a list containing these items.
+//Task 20
+var languages = ["English", "Urdu", "Balochi", "Arabic"];
+console.log("List Of langueges:");
+for (var _i = 0, languages_1 = languages; _i < languages_1.length; _i++) {
+    var top_1 = languages_1[_i];
+    console.log(top_1);
+}
